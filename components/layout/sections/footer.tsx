d1d461df43435ad276af52a0a80dee619d086a14
David Orestes León Dueñas <davidleond99@gmail.com>
2024-11-30 17:22:37 -0500

    updated style

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export const FooterSection = () => {
   return (
     <footer id="footer" className="container py-24 sm:py-32">
-      <div className="p-10 bg-card border border-primary rounded-2xl">
+      <div className="p-10 bg-card border border-secondary rounded-2xl">
         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
           <div className="col-span-full xl:col-span-2">
             <Link href="#" className="flex font-bold items-center">
-              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-primary" />
+              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
 
               <h3 className="text-2xl">Shadcn</h3>
             </Link>
